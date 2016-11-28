e5d190e80eae6ac8320cb2eaf15349813dcd0aec
MimiSan <mimisan@bitnet.me>
2016-11-28 01:13:38 +0100

    Projects

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+  });
+});
+
+
 // /bad - send back json with errorMessage "Error handeling request"
 app.get('/bad', (req, res) => {
   res.send({
